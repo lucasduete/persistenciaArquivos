94cd0f1f2afa737cd8e2c18957866a10e233ee01
Lucas Duete <lm.duete@gmail.com>
2017-09-18 09:34:37 -0300

    Aula 18/09 pt2

---
+++ src/io/github/lucasduete/percistenciaArquivos/GenericDaoTexto.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.lucasduete.percistenciaArquivos;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author lucasduete
+ */
+public class GenericDaoTexto <T>{
+    
+    private File arquivo;
+    
+    public GenericDaoTexto(String path) throws IOException {
+        arquivo = new File(path);
+        
+        if(!arquivo.exists()) 
+            arquivo.createNewFile();
+    }
+    
+    public boolean salvar(T obj) throws IOException {  
+        BufferedWriter writer = new BufferedWriter(
+            new PrintWriter(
+                new FileWriter(arquivo, true), true));
+        
+        writer.write(obj.toString());
+        writer.newLine();
+            
+        writer.close();        
+        return true;
+    }
+    
+    public List<T> listar() {
+        List<T> list = null;
+        
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(arquivo));
+            
+            String linha = reader.readLine();
+            list = new ArrayList<>();
+            
+            while(linha != null) {
+                T obj = (T) linha;
+                list.add(obj);
+                linha = reader.readLine();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return list;
+    }
+    
+}
