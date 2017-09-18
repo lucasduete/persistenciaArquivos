3a059062f65c62bf94f0a970548e5298ae21bf1e
Lucas Duete <lm.duete@gmail.com>
2017-09-18 13:37:35 -0300

    Aula 18/09 pt3

--- src/io/github/lucasduete/percistenciaArquivos/GenericDaoTexto.java
+++ src/io/github/lucasduete/percistenciaArquivos/GenericDaoTexto.java
@@ -14,8 +14,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
